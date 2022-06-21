4673253214d0dcb08b81185e313f756ac4dca26a
Lucas França Marinho <99843203+LucasMarinho53@users.noreply.github.com>
2022-06-21 15:33:59 -0300

    Melhorando o design "Perfil"

--- root.session.sql
+++ root.session.sql
@@ -7,7 +7,7 @@ CREATE TABLE Cliente (
  CPF VARCHAR(11) NOT NULL,  
  Nome VARCHAR(150) NOT NULL,  
  Email VARCHAR(150) NOT NULL,
- Senha INT
+ Senha VARCHAR(150) NOT NULL
 ); 
 
 SELECT * FROM Cliente;
@@ -52,7 +52,7 @@ DROP TABLE Cliente;
 
 select * from Cliente;
 
-select ID_Venda , ValorTotal , Mes_venda from Venda;
+select * from Venda;
 
 INSERT INTO Produtos(id,Nome, Preço, Tema) values
 ("01",'Fantasminhas do Pac-man', 'R$ 80,99', 'Geek'), ("02",'Star Wars -R2D2', 'R$ 67,99', 'Geek'), ("03",'Herói e Vilão de Gotham City - Batman e Coringa', 'R$ 88,99', 'Geek'),("04",'Super Mario Bros', 'R$ 96,99', 'Geek'), 
@@ -60,4 +60,14 @@ INSERT INTO Produtos(id,Nome, Preço, Tema) values
 ("09",'Urso Arqueiro', 'R$ 105,99', 'Kids'), ("10",'Viagem ao espaço', 'R$ 66,99', 'Kids'), ("11",'Aventuras em alto Mar', 'R$ 87,99', 'Kids'), ("12",'Fada dos Desejos', 'R$ 88,99', 'Kids'),
 ("13",'Parque Central', 'R$ 80,99', 'Room'), ("14",'Na Beira da Praia', 'R$ 96,99', 'Room'), ("15",'Por do Sol', 'R$ 67,99', 'Room'), ("16",'Amanhecer Nas Neves', 'R$ 88,99', 'Room');
 
-INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("1","12","350","Fevereiro","5");
+INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("5","17","290","Janeiro","3");
+INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("6","10","250","Janeiro","2");
+INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("7","1","230","Janeiro","3");
+INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("8","13","370","Novembro","5");
+INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("9","3","80","Novembro","1");
+INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("10","6","70","Novembro","1");
+INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("11","8","90","Junho","1");
+INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("12","4","50","Junho","1");
+INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("13","9","300","Julho","4");
+INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("14","15","250","Agosto","2");
+INSERT INTO Venda(ID_Venda,ID_produto,ValorTotal,Mes_Venda,Quantidadevenda) VALUES ("15","19","150","Setembro","2");
